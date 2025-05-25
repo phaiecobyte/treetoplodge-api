a6a3cf4edb762584ff01537d53f347a185bf7f0c
phaiecobyte <brave6823@gmail.com>
2025-05-25 23:55:54 +0700

    Initial commit

---
+++ src/main/java/com/treetoplodge/treetoplodge_api/TreetoplodgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.treetoplodge.treetoplodge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreetoplodgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreetoplodgeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treetoplodge/treetoplodge_api/TreetoplodgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treetoplodge.treetoplodge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreetoplodgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
